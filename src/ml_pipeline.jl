5a1d7bc78c4b8d1cc13baf2b25f7f213b396a40c
nicolofp <nicolo.fp@gmail.com>
2024-06-28 20:19:57 -0400

    update lasso

--- src/ml_pipeline.jl
+++ src/ml_pipeline.jl
@@ -4,24 +4,40 @@ using Distributions, StatsBase, Random
 
 # Read object in julia format 
 DT = load_object("data/pheno.jld2")
+Mt = load_object("data/met.jld2")
+
+# Explore metabolites
+Mt_details = describe(Mt)
+met_names = Mt_details[Mt_details.nmissing .== 0 ,:variable]
+cv_variation = 100*std.(eachcol(Mt[:,met_names[2:655]])) ./ mean.(eachcol(Mt[:,met_names[2:655]]))
+Mt_cv = DataFrame(names = met_names[2:655],cv = cv_variation)
+sort(Mt_cv,:cv, rev = true)
+
+# Join dataframes
+DT = innerjoin(DT,Mt[:,met_names], on = :sample_id => :sample)
 
 # Remove lines with missing data 
 dropmissing!(DT)
-
+unique!(DT)
 describe(DT)
 schema(DT)
-coerce!(DT, :gender=>OrderedFactor, :race=>OrderedFactor, 
-            :smoking_status=>OrderedFactor, :center=>OrderedFactor)
 
-train, test = partition(collect(eachindex(DT.sample_id)), 0.8, shuffle=true, rng=418)
-X = DT[:,vcat(2:5,7:10)]
+train, test = partition(collect(eachindex(DT.sample_id)), 0.8, shuffle=true, rng=438)
+X = DT[:,vcat(2:5,7:664)]
 y = DT.fev1_post;
 
 # Machine learning pipeline
 RandomForest = MLJ.@load RandomForestRegressor pkg=DecisionTree
+LassoRegressor = MLJ.@load LassoRegressor pkg=MLJLinearModels
 rf_model = RandomForest();
+la_model = LassoRegressor(solver = MLJLinearModels.ProxGrad(max_iter = 10000))
 
-rf_leaf = range(rf_model, :min_samples_leaf, lower = 10, upper = 30)
+pipe_transformer = (X -> coerce!(X, :gender=>OrderedFactor, 
+                                     :race=>OrderedFactor,
+                                     :smoking_status=>OrderedFactor, 
+                                     :center=>OrderedFactor)) |> ContinuousEncoder()
+
+rf_leaf = range(rf_model, :min_samples_leaf, lower = 5, upper = 20)
 rf_mtry = range(rf_model, :n_subfeatures, lower = 1, upper = 8)
 rf_tree = range(rf_model, :n_trees, values = [100, 250, 500, 1000, 2500])
 rf_tm = TunedModel(model = rf_model, 
@@ -29,10 +45,21 @@ rf_tm = TunedModel(model = rf_model,
                    resampling = CV(nfolds = 5, rng = 123), 
                    ranges = [rf_leaf, rf_mtry, rf_tree],
                    measure = rms)
-rf_mtm = machine(rf_tm, X, y)
+                   
+pipe = pipe_transformer |> rf_tm
+pipe_shrinkage = pipe_transformer |> la_model
+
+rf_mtm = machine(pipe_sh, X, y)
 fit!(rf_mtm, rows = train);
+
+lm_lasso = machine(pipe_shrinkage, X, y)
+fit!(lm_lasso, rows = train)
+evaluate!(lm_lasso, resampling = CV(nfolds=10, rng=1234), measure = [rsquared, rmse])
 
 predictions_rf = MLJ.predict(rf_mtm, rows=test);
 root_mean_squared_error(y[test],predictions_rf)
+
+predictions_sh = MLJ.predict(lm_lasso, rows=test);
+root_mean_squared_error(y[test],predictions_sh)
 
 #scatter(y[test],predictions_rf)
